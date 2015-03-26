f939b62c1c89396e66326e7866bf3b6ed557ab28
Shah Zafrani <szafran2@spsu.edu>
2015-03-26 16:04:21 -0400

    Added recursive tree traversal functions. All that is left is to add a step counter.

--- RecoveredXMLParser/src/BehaviorTree.java
+++ RecoveredXMLParser/src/BehaviorTree.java
@@ -10,52 +10,49 @@ public class BehaviorTree {
     Node root;
     Scanner userInput = new Scanner(System.in);
 
-    public BehaviorTree(Node rt){
+    public BehaviorTree(Node rt) {
         root = rt;
     }
-    public void printTree(){ // does a bit of formatting, then calls printChildren on the root of the tree
+
+    public void printTree() { // does a bit of formatting, then calls printChildren on the root of the tree
         System.out.println();
         System.out.println();
         System.out.println("Printing Behavior Tree Structure...");
         printChildren(root);
     }
+
     public void printChildren(Node n) // prints children of this node and recurses through their children
     {
         System.out.println("Children of: " + n.identifier);
-        for(Node c: n.children){
-            if(c.children.isEmpty())
-            {
+        for (Node c : n.children) {
+            if (c.children.isEmpty()) {
                 System.out.println(c.identifier);
-            }
-            else {
-                System.out.println(c.identifier+ " is a child of: " + n.identifier);
+            } else {
+                System.out.println(c.identifier + " is a child of: " + n.identifier);
                 printChildren(c);
             }
         }
     }
-    public String printPrompt()
-    {
+
+    public String printPrompt() {
         System.out.println("enter the behavior you would like me to respond to:");
         return userInput.nextLine();
     }
+
     public void printInstructions() {
         System.out.println("Would you like to search using Breadth-first, or Depth-first?");
         System.out.println("enter \"b\" for BREADTH-first, or \"d\" for DEPTH-first");
         System.out.println("enter \"x\" to EXIT");
     }
-    public String chooseRandomResponse(Node n){
+
+    public String chooseRandomResponse(Node n) {
         Random rand = new Random();
         ArrayList<String> possibilities = new ArrayList<String>();
-        for (Node c : n.children)
-        {
-            if (c.isResponse == true)
-            {
+        for (Node c : n.children) {
+            if (c.isResponse == true) {
                 possibilities.add(c.identifier);
-            }
-            else if (c.isResponse == false)
-            {
-                for (Node g : c.children)
-                {
+            } else if (c.isResponse == false) {
+                for (Node g : c.children) {
                     possibilities.add(g.identifier);
                 }
             }
@@ -63,43 +60,37 @@ public class BehaviorTree {
         int r = rand.nextInt(possibilities.size());
         return possibilities.get(r);
     }
-    public void respondUserInput()
-    {
+
+    public void respondUserInput() {
         printInstructions();
         boolean timeToQuit = false;
-        while (!timeToQuit)
-        {
+        while (!timeToQuit) {
             String input = userInput.nextLine();
-            if  (input.contains("b"))
-            {
-                System.out.println(breadthFirst());
+            if (input.contains("b")) {
+                System.out.println("Response: " + breadthFirst());
                 printInstructions();
-            }
-            else if (input.contains("d"))
-            {
-                System.out.println(depthFirst());
+            } else if (input.contains("d")) {
+                System.out.println("Response: " + depthFirst());
                 printInstructions();
-            }
-            else if (input.contains("x"))
-            {
+            } else if (input.contains("x")) {
                 timeToQuit = true;
-            }
-            else
-            {
+            } else {
                 respondUserInput();
                 timeToQuit = true;
             }
         }
     }
-    public String breadthFirst()
-    {
+
+    public String breadthFirst() {
         System.out.println("You have chosen Breadth-First");
         String s = printPrompt();
 
-        return findBreadthFirst(s);
+        return recurseBreadthFirst(s, root);
+        //return findBreadthFirst(s);
     }
-    public String findBreadthFirst(String request) /*this is where I hit crunch time. I will have filled in this psuedo code with working methods,
-    and will update this to use recursion as I am basically doing the same thing over and over again.*/
+
+    /*public String findBreadthFirst(String request) /*this is where I hit crunch time. I will have filled in this psuedo code with working methods,
+    and will update this to use recursion as I am basically doing the same thing over and over again.*//*
     {
         for (Node n : root.children){
             if (n.identifier.equalsIgnoreCase(request)) {
@@ -117,16 +108,42 @@ public class BehaviorTree {
             }
         }
         return "unable to process \"" + request + "\"";
+}*/
+    public String recurseBreadthFirst(String request, Node current) {
+        for (Node n : current.children) {
+            if (n.identifier.equalsIgnoreCase(request)) {
+                return chooseRandomResponse(n);
+            }
+        }
+        for (Node n : current.children) {
+            if (recurseBreadthFirst(request, n) != "null") {
+                return recurseBreadthFirst(request, n);
+            }
+        }
+        return "null";
     }
-    public String depthFirst()
-    {
+
+    public String depthFirst() {
         System.out.println("You have chosen Depth-First");
         String s = printPrompt();
-
-        return findDepthFirst(s);
+        return recurseDepthFirst(s, root);
+        //return findDepthFirst(s);
     }
-    public String findDepthFirst(String request) /*this is where I hit crunch time. I will have filled in this psuedo code with working methods,
-    and will update this to use recursion as I am basically doing the same thing over and over again.*/
+
+    public String recurseDepthFirst(String request, Node current) {
+        if (current.identifier.equalsIgnoreCase(request)) {
+            return chooseRandomResponse(current);
+        } else {
+            for (Node n : current.children) {
+                if (recurseDepthFirst(request, n) != "null") {
+                    return recurseDepthFirst(request, n);
+                }
+            }
+        }
+        return "null";
+    }
+    /*public String findDepthFirst(String request) /*this is where I hit crunch time. I will have filled in this psuedo code with working methods,
+    and will update this to use recursion as I am basically doing the same thing over and over again.*//*
     {
         for (Node n : root.children) {
             if (n.identifier.equalsIgnoreCase(request)){
@@ -153,5 +170,5 @@ public class BehaviorTree {
             }
         }
         return "unable to process \"" + request + "\"";
-    }
+    }*/
 }
